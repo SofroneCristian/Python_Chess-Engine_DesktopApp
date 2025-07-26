80e2bfd600c74ddbae128cfd6d0a35fabb0d3ef6
SofroneCristian <150044473+SofroneCristian@users.noreply.github.com>
2025-07-26 19:45:37 +0300

    Add files via upload

---
+++ player.py
@@ -0,0 +1,13 @@
+import chess
+
+class Player:
+    def __init__(self, color, board):
+        self.color = color
+        self.board = board  # Referinta board din python-chess
+        
+        
+
+
+    
+
+
